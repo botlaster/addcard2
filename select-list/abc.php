24a7adb495801e85c38b85b46bc04d401ad93978
Metha <botlaster@gmail.com>
2018-07-15 22:31:48 +0700

    first backup

---
+++ select-list/abc.php
@@ -0,0 +1,26 @@
+﻿<?php
+		 $_SESSION["intLine"] = 0;
+		 $_SESSION["strProductID"][0] = $_POST["txtProductID"];
+		 $_SESSION["strQty"][0] = $_POST["txtQty"];
+
+		 header("location:show.php");
+	}
+}
+else
+{
+	
+	$key = array_search($_POST["txtProductID"], $_SESSION["strProductID"]);
+	if((string)$key != "")
+	{
+		 $_SESSION["strQty"][$key] = $_SESSION["strQty"][$key] + $_POST["txtQty"];
+	}
+	else
+	{
+		
+		 $_SESSION["intLine"] = $_SESSION["intLine"] + 1;
+		 $intNewLine = $_SESSION["intLine"];
+		 $_SESSION["strProductID"][$intNewLine] = $_POST["txtProductID"];
+		 $_SESSION["strQty"][$intNewLine] = $_POST["txtQty"];
+	}
+	
+	 header("location:show.php");
